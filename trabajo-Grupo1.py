478305bc1e927566ad8fda5cd94c3dec8381d4ea
EngelsVAC <coalenciso@gmail.com>
2026-04-19 13:07:43 -0500

    Refactor divisibility functions into a class

--- trabajo-Grupo1.py
+++ trabajo-Grupo1.py
@@ -1,17 +1,25 @@
-def calc_divisores(n):
-  divisores= []
-  for i in range (1, n+1):
-    if n%1 == 0:
-      divisores.append(i)
-  return divisores 
+from matplotlib import pyplot as plt
+from abc import ABC, abstractmethod
 
-def rel_divisibilidad(c):
-  pares= []
-  for i in c:
-    for e in c:
-      if b % a == 0:
-        pares.append((a,b))
-  return pares
+class Divisivilidad(ABC): 
+   def __str__(self, n):  
+       self.__n = n
+       self.__Divisor = CalcularDivisores()
+       self.__A = []
+      def calc_divisores(n):
+        divisores= []
+        for i in range (1, n+1):
+          if n%1 == 0:
+            divisores.append(i)
+        return divisores 
+      
+      def rel_divisibilidad(c):
+        pares= []
+        for i in c:
+          for e in c:
+            if b % a == 0:
+              pares.append((a,b))
+        return pares
 
 def main ():
   print("Relacion de divisibilidad")
