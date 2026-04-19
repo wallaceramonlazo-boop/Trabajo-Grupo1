20ac7f0615444bdd9732343291ed5dd6c6c6ff29
EngelsVAC <coalenciso@gmail.com>
2026-04-19 13:14:36 -0500

    Refactor Divisivilidad class and methods
    Refactor the Divisivilidad class methods and improve divisor calculation.

--- trabajo-Grupo1.py
+++ trabajo-Grupo1.py
@@ -6,27 +6,21 @@ class Divisivilidad(ABC):
        self.__n = n
        self.__Divisor = CalcularDivisores()
        self.__A = []
-      def calc_divisores(n):
-        divisores= []
-        for i in range (1, n+1):
-          if n%1 == 0:
-            divisores.append(i)
-        return divisores 
       
-      def rel_divisibilidad(c):
-        pares= []
-        for i in c:
-          for e in c:
-            if b % a == 0:
-              pares.append((a,b))
-        return pares
-
+   def CalcularDivisores(self):
+      return [i for i in range(1, self.__n + 1) if self.__n % i == 0]
+   def rel_divisibilidad(self):
+     pares = CalcularDivisores()
+     
+   def MostrarConsola(self):
+     for i in self.__A:
+       print("kbro")
+#Menu
 def main ():
   print("Relacion de divisibilidad")
   try:
-    n = int(input("Ingrese un numero n: "))
+    Maximo = int(input("Ingrese un numero n: "))
+    Control = Divisivilidad(Maximo)
     if n <= 0:
       print("Ingrese un numero mayor a cero ")
-      
-    d= calc_divisores(n)
-    print(f"Divisores de {n} {d}")
+       
