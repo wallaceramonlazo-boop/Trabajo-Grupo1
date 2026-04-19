9e5d056d17ad21864e11dff3b82b1d3c2651ffdc
wallaceramonlazo-boop <wallaceramonlazo@gmail.com>
2026-04-19 14:05:20 -0500

    Update trabajo-Grupo1.py

--- trabajo-Grupo1.py
+++ trabajo-Grupo1.py
@@ -1,26 +1,45 @@
 from matplotlib import pyplot as plt
 from abc import ABC, abstractmethod
 
-class Divisivilidad(ABC): 
-   def __str__(self, n):  
-       self.__n = n
-       self.__Divisor = CalcularDivisores()
-       self.__A = []
-      
-   def CalcularDivisores(self):
-      return [i for i in range(1, self.__n + 1) if self.__n % i == 0]
-   def rel_divisibilidad(self):
-     pares = CalcularDivisores()
-     
-   def MostrarConsola(self):
-     for i in self.__A:
-       print("kbro")
-#Menu
-def main ():
-  print("Relacion de divisibilidad")
-  try:
-    Maximo = int(input("Ingrese un numero n: "))
-    Control = Divisivilidad(Maximo)
-    if n <= 0:
-      print("Ingrese un numero mayor a cero ")
+
+class Divisibilidad(ABC):
+    def __init__(self, n):
+        self.__n = n
+        self.__Divisores = self.CalcularDivisores()
+
+    def CalcularDivisores(self):
+        return [i for i in range(1, self.__n + 1) if self.__n % i == 0]
+
+    def rel_divisibilidad(self):
+        return self.CalcularDivisores()
+
+    def MostrarConsola(self):
+        print(f"Divisores de {self.__n}:")
+        for i in self.__Divisores:
+            print(i)
+   
+    @abstractmethod
+    def mostrar(self):
+        pass
+
+
+class DivisibilidadConcreta(Divisibilidad):
+    def mostrar(self):
+        self.MostrarConsola()
+
+
+def main():
+    print("Relacion de divisibilidad")
+    try:
+        Maximo = int(input("Ingrese un numero n: "))
+        if Maximo <= 0:
+            print("Ingrese un numero mayor a cero")
+        else:
+            Control = DivisibilidadConcreta(Maximo)
+            Control.mostrar()
+    except ValueError:
+        print("Ingrese un numero valido")
+
+
+main()
        
